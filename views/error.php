fa7d8fadd27b5016146d5904425ea1e39565e228
Ulyana Shumilina <shoomilka@i.ua>
2020-02-09 16:11:27 +0200

    fix

--- views/error.php
+++ views/error.php
@@ -2,7 +2,7 @@
 require('header.php');
 ?>
 <p><b>Error!&nbsp;</b>
-Wrong page! Go to the&nbsp;<a href="/index.php/list">main page</a>.</p>;
+Wrong page! Go to the&nbsp;<a href="/index.php/list">main page</a>.</p>
 
 <?php
 require('footer.php');
